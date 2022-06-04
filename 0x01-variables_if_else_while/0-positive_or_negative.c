cf87b5c55144a2d4b18e950e01b0210f7977e401
ammanu2et <ammantd2@gmail.com>
2022-06-04 07:05:54 -0700

    c variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -19,7 +19,7 @@ if (n == 0)
 {
 printf("%i is zero\n", n);
 }
- 
+
 else if (n < 0)
 {
 printf("%i is negative\n", n);
@@ -29,7 +29,7 @@ else
 {
 printf("%i is positive\n", n);
 }
-	     
+
 return (0);
 
 }
