a9deb4ec3f95a5ebd8a798d54ae025268dd3bf87
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-06 17:33:26 +0300

    c variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,11 +7,11 @@
 int main(void)
 
 {
-char ch;
+char alphabet;
 
-for (ch = 'a'; ch <= 'z'; ch++)
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
 {
-putchar(ch);
+putchar(alphabet);
 }
 
 putchar('\n');
