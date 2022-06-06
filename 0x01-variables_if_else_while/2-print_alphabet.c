cacde60c1d4e3d0cfb26ee2a0eddda9390a14db2
ammanu2et <ammantd2@gmail.com>
2022-06-06 01:19:12 -0700

    c variables

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/*
+ *All lower alphabets print
+ *
+ *all out put correct
+ */
+
+void main()
+
+{
+int ch;
+for (ch= 'a'; ch<= 'z'; ch++)
+
+putchar(ch);
+putchar ('\n');
+return(0);
+}
