7154868b3b43e971d43ae68922f379152f5dcae7
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-07 00:44:13 +0300

    c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,10 @@
+include <stdio.h>
+/**
+ * Description: main - Prints out a string to stdout.
+ * Return: 0 if success.
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\", 59);
+return (1);
+}
