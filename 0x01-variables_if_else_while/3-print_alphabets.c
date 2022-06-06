af129efbc885caa4e8723ad42ac1d3c57c647dac
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-06 17:57:00 +0300

    c variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main -entry point
+ *
+ *Return: always 0
+ */
+int main(void)
+
+{
+char alphabet;
+
+for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+for (alphabet = 'A'; alphabet <= 'Z'; alphabet++)
+{
+putchar(alphabet);
+}
+
+putchar('\n');
+
+return (0);
+
+}
