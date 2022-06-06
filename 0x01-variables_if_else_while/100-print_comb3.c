6eb39e1e65e2f37a25ce2c4740f0087f77c1c683
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-06 23:11:09 +0300

    c variables

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+ *main -entry point
+ *
+ *Return: always 0
+ */
+int main(void)
+
+{
+int i, j;
+
+for (i = 0; i < 9; i++)
+{
+for (j = 0; j < 10; j++)
+{
+putchar((i % 10) + '0');
+putchar((j % 10) + '0');
+
+if (i == 8 && a == 9)
+continue;
+
+putchar(',');
+putchar(' ');
+}
+}
+putchar('\n');
+
+return (0);
+
+}
