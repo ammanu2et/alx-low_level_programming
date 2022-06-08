b9e2240b8579c0c70365cb2f8d6a0a2854e05afe
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-08 21:58:13 +0300

    c functions

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include <main.h>
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+char c = 'a';
+
+do {
+putchar(c);
+c++;
+}
+while (c <= 'z');
+putchar('\n');
+}
