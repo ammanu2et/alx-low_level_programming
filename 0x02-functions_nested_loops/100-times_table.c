ee2baafed93453eab2ab2c206fd7ef0041902cf2
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-11 21:53:13 +0300

    c nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,12 +7,12 @@
  */
 void print_times_table(int n)
 {
-int j,i,d;
-if(n > 15 || n < 0)
+int j, i, d;
+if (n > 15 || n < 0)
 return;
-for(i = 0; i <= n; i++)
+for (i = 0; i <= n; i++)
 {
-for(j = 0; j <= n; j++)
+for (j = 0; j <= n; j++)
 {
 
 d = i * j;
