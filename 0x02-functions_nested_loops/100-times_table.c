67d2a9e83ac196cde1c470a894a8ded4f2c7830c
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-11 14:58:41 +0300

    c nested loops

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+int n, i;
+for (i = 0; i <= 15; ++i)
+{
+
+    printf("%d * %d = %d\n", n, i, n * i);
+    printf('\n');
+}
+    return (0);
+}
