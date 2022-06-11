29a4c1170ecbef2fb8c278f93f9b60aeb453ce77
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-11 15:58:22 +0300

    c nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,7 +16,7 @@ void print_times_table(int n)
              else
              {
                 d = i * j;
-   	           _putchar(d + '0');
+   	           _putchar(d,  + '0');
    	         }
 
          }
