4338257eed7da4b4f6a634231e12cf0c41641382
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-11 21:51:40 +0300

    c nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,48 +1,49 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * times_table - Prints the 9 times table, starting with 0
+ * print_times_table - Prints the n times table, starting with 0.
+ * @n: int type number
  * Return: empty output
  */
 void print_times_table(int n)
 {
-      int j,i,d;
-      if(n > 15 || n < 0)
-            return;
-      for(i = 0; i <= n; i++)
-      {
-        for(j = 0; j <= n; j++)
-          {
+int j,i,d;
+if(n > 15 || n < 0)
+return;
+for(i = 0; i <= n; i++)
+{
+for(j = 0; j <= n; j++)
+{
 
-            d = i * j;
-            if (d > 99)
-            	{
-            		_putchar(d / 100 + '0');
-            		_putchar((d / 10 % 10) + '0');
-            		_putchar(d % 10 + '0');
-           		}
-           	else if (d > 9)
-          		{
-            		_putchar(' ');
-            		_putchar(d / 10 + '0');
-            		_putchar(d % 10 + '0');
-            	}
-            else if (j != 0)
-            	{
-            		_putchar(' ');
-            		_putchar(' ');
-            		_putchar(d + '0');
-            	}
-            else
-            		_putchar(d + '0');
+d = i * j;
+if (d > 99)
+{
+_putchar(d / 100 + '0');
+_putchar((d / 10 % 10) + '0');
+_putchar(d % 10 + '0');
+}
+else if (d > 9)
+{
+_putchar(' ');
+_putchar(d / 10 + '0');
+_putchar(d % 10 + '0');
+}
+else if (j != 0)
+{
+_putchar(' ');
+_putchar(' ');
+_putchar(d + '0');
+}
+else
+_putchar(d + '0');
 
-            	if (j != n)
-            		{
-            		    _putchar(',');
-            			_putchar(' ');
-            		}
-          }
-            _putchar('\n');
+if (j != n)
+{
+_putchar(',');
+_putchar(' ');
+}
+}
+_putchar('\n');
 
-      }
+}
 }
