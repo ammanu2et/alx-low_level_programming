7c36a6946a537934a92a72b08373363875e8aaa9
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-11 15:36:57 +0300

    c nested loops

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -3,9 +3,10 @@
  * times_table - Prints the 9 times table, starting with 0
  * Return: empty output
  */
-void print_times_table(int n);
+void print_times_table(int n)
 {
       int j,i,n;
+
       for(i=0;i<=n;i++)
       {
         for(j=0;j<=n;j++)
