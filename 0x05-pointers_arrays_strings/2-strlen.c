e981cd898e1c421967a3ce54fdae8fefb8a67246
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-13 16:48:59 +0300

    c pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swaps two integers.
+ * @a: first variable.
+ * @b: second variable.
+ */
+int _strlen(char *s);
+{
+int strlen(char *s)
+}
