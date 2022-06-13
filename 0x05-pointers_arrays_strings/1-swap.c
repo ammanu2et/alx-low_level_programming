b075887958b232c20036b2fcf2fa4c49cd6bbb44
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-13 16:31:40 +0300

    c pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * swap_int - swaps two integers.
+ * @: the variable that's going to be changed.
+ */
+void swap_int(int *a, int *b);
+{
+int n;
+
+n = *a
+*a = *b
+*b = n
+}
