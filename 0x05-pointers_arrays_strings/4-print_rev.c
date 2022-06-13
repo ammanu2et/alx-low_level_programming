39e806077b83a82b244d351e88729ee1262ce48d
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-13 22:33:44 +0300

    c pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+#include <stdio.h>
+/**
+ * _puts - prints a string.
+ * @str: a pointer to an int
+ * Return: void and correct
+ */
+void print_rev(char *s)
+{
+if (*s)
+{
+reverse(str+1);
+putchar ("%c", *s);
+}
+putchar ('\n');
+}
