b719fec4c43a3e022204ad83c920125f1cb245d1
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-13 20:55:28 +0300

    c pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <string.h>
+/**
+ *  - length of a string.
+ * @s: a pointer to an int
+ * Return: void and correct
+ */
+void _puts(char *str)
+{
+int len = strlen(str) + 1;
+char *temp alloca(len);
+int i;
+for (i = 0; i < len; i++)
+{
+char ch = str[i];
+if (ch == '\n')
+break;
+temp[i] = ch;
+}
+temp[i] = 0;
+puts(temp);
+}
