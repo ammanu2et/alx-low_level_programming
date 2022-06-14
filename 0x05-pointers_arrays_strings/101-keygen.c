0cbb9dc8b8098ee21069610906d64458d0f39b1c
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-14 23:31:39 +0300

    c pointers, arrays and strings

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,45 +1,16 @@
-#include <math.h>
-#include <stdio.h>
+#include "main.h"
 #include <stdlib.h>
 #include <time.h>
+#include <stdio.h>
+/**
+ */
+int main(void)
+{
+int num;
 
-void randompasswordgeneration(int n)
-{
-int i = 0;
-int randomizer = 0;
-srand((unsigned int)(time(NULL)));
-char numbers[] = "0123456789";
-char letter[] = "abcdefghijklmnopqrstuvwxyz";
-char LETTER[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-char symbols[] = "!@#$%^&*?";
-char password[n];
-randomizer = rand() % 4;
+srand(time(0));
+num = rand();
+printf("%i\n", num);
+return (0);
 
-for (i = 0; i < n; i++)
-{
-if (randomizer == 1)
-{
-password[i] = numbers[rand() % 10];
-randomizer = rand() % 4;
-printf("%c", password[i]);
-}
-else if (randomizer == 2)
-{
-password[i] = symbols[rand() % 8];
-randomizer = rand() % 4;
-printf("%c", password[i]);
-}
-else if (randomizer == 3)
-{
-password[i] = LETTER[rand() % 26];
-randomizer = rand() % 4;
-printf("%c", password[i]);
-}
-else
-{
-password[i] = letter[rand() % 26];
-randomizer = rand() % 4;
-printf("%c", password[i]);
-}
-}
 }
