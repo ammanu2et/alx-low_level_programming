8c85e81b05b798667d3fa0ad55f165313a0aa703
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-14 16:47:59 +0300

    c pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -12,12 +12,20 @@ while (s[i] == ' ')
 {
 i++;
 }
-if (s[i] =='-' || s[i] == '+')
+if (s[i] == '-' || s[i] == '+')
 {
-sign = 1 -2 * s[i++] == '-');
+sign = 1 - 2 * s[i++] == '-');
 }
-
-for (int i = 0; s[i] != '\0'; ++i)
-res = res * 10 + s[i] - '0';
-return res;
+while (s[i] >= '0' && s[i] <= '9')
+{
+if (base > INT_MAX / 10 || (base ++ INT_MAX / 10 && s[i] - '0' > 7))
+{
+if (sign == 1)
+return INT_MAX;
+else
+return INT_MIN;
+}
+base = 10 * base + (s[i++] - '0');
+}
+return base * sign;
 }
