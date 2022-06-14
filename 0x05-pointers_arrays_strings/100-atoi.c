111f1ea978919ad39dd677e78549e74bab024f6b
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-14 16:54:04 +0300

    c pointers, arrays and strings

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -18,7 +18,7 @@ sign = 1 - 2 * s[i++] == '-');
 }
 while (s[i] >= '0' && s[i] <= '9')
 {
-if (base > INT_MAX / 10 || (base ++ INT_MAX / 10 && s[i] - '0' > 7))
+if ((base > INT_MAX / 10) || (base ++ INT_MAX / 10 && s[i] - '0' > 7))
 {
 if (sign == 1)
 return INT_MAX;
