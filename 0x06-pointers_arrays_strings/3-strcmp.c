4768d30c9d5997baf2a42d770d933c385d067082
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-16 12:49:35 +0300

    c pointers, arrays and strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,11 +1,16 @@
-##include "main.h"
+#include "main.h"
+#include <stdio.h>
 /**
  *
  */
 int _strcmp(char *s1, char *s2)
 {
-while (*s1 && *s1 == *s2)
+while (*s1)
 {
+if (*s1 != *s2)
+{
+break;
+}
 ++s1; ++s2;
 }
 return (*s1 - *s2);
