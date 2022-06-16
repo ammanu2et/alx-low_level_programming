c936cfdc6e64039be7a19656cc990a1234452d75
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-16 12:17:49 +0300

    c pointers, arrays and strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,15 +1,15 @@
-##include "main.h"
+#include "main.h"
+#include <string.h>
 /**
  *
  */
 char *_strncat(char *dest, char *src, int n)
 {
-int i;
-i = 0;
-while (dest_len[i] != '\0'; i++)
-
-for (i = 0; i < n && src[i] != '\0'; i++)
-dest[dest_len[i] + 1] = src[i];
-dest[dest_len[i]] = '\0';
-return dest;
+char *p = dest + strlen(dest);
+while (*src != '\0' && n--)
+{
+*p++ = *src++;
+}
+*p = '\0';
+return (dest);
 }
