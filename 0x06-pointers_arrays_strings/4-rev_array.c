353372e5626d1b18a9cb6033a7adb616e5a29286
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-16 15:30:28 +0300

    c pointers, arrays and strings

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -4,15 +4,14 @@
  */
 void reverse_array(int *a, int n)
 {
-int *p;
+int aux[n];
 
 for (int i = 0; i < n; i++)
 {
-p[n - 1 - i] = a[i];
+aux[n - 1 - i] = a[i];
 }
 for (int i = 0; i < n; i++)
 {
-a[i] = p[i];
+a[i] = aux[i];
 }
-return (0);
 }
