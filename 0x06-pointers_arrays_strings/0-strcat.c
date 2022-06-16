c5099f8b1b782339eab20b034d631f70acd98c88
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-16 11:53:27 +0300

    c pointers, arrays and strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -2,7 +2,8 @@
 #include <string.h>
 /**
  * _strcat - appends a copy of one string to another.
- *
+ * dest - destination
+ * src - source string
  * Return: returns the string of destination file.
  */
 char *_strcat(char *dest, char *src)
