761111639b1ee88c31f133d1939a42f64c5a937a
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-17 15:04:22 +0300

    c pointers, arrays and strings

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *
+ */
+char *rot13(char *n)
+int i;
+{
+if (n >= 65 && n <= 90)
+{
+n = 65 + (n - 65 + 13) % 26
+}
+else if (n >= 97 && n<= 122)
+{
+n = 97 + (n - 97 + 13) % 26
+}
+return (n);
+}
