dd67957f3605da8d953e339ce0ece3e95f016015
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-21 14:26:26 +0300

    c pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -17,9 +17,9 @@ else
 {
 i = n;
 }
-for (i = 0, i > 10, i = i / 10);
-
-for (j = 1000, j < 10, j = i / 10);
+for (i = 0, i >= 10, i = i / 10);
+i = j;
+for (j = 1000, j >= 10, j = j / 10);
 
 _putchar (i % 10);
 
