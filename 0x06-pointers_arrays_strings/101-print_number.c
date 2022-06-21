0ac6a7eac9209ae7b99420391d8c01e9402c428c
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-21 21:56:34 +0300

    c pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -3,33 +3,27 @@
 /**
  * print_number - prints an integer
  * @n: input an integer
- * Return: no return here
+ * Return: no return
  */
 void print_number(int n)
 {
-	unsigned int x, y, count;
-
-	if (n < 0)
-	{
-		_putchar(45);
-		x = n * -1;
-	}
-	else
-	{
-		x = n;
-	}
-
-	y = x;
-	count = 1;
-
-	while (y > 9)
-	{
-		y /= 10;
-		count *= 10;
-	}
-
-	for (; count >= 1; count /= 10)
-	{
-		_putchar(((x / count) % 10) + 48);
-	}
+int y, i, j, x = 1;
+if (n < 0)
+{
+_putchar(45);
+i = n * -1;
+}
+else
+{
+i = n;
+}
+for (y = 0; i >= 10; y++)
+{
+i /= 10;
+x *= 10;
+}
+for (j = x; j >= 1; j /= 10)
+{
+_putchar((n / j) % 10);
+}
 }
