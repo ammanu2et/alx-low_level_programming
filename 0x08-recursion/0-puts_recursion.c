1fcef4423722dc5fcfb605533b83fdb06beadac7
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-23 01:02:52 +0300

    c recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -5,10 +5,11 @@
  *
  */
 void _puts_recursion(char *s)
-if (s.length == 0)
 {
-return(0);
+if (*s == '\0')
+return ;
+printf("%c", *s);
+_puts_recursion (s + 1);
 }
-{
-return (s[s.length - 1] + reverse(s.substr(0, s.length - 1)));
-}
+
+
