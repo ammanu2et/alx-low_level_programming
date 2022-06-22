b4a8ad00667c8eefe59056615430ec5dde8c2005
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-06-23 02:44:51 +0300

    c recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,7 +1,9 @@
 #include "main.h"
 /**
- *
- *
+ * _pow_recursion - calculates the power of a number.
+ * @x: the base
+ * @y: the power
+ * Return: -1 if less than y = 0, 1 if y =0 and the result...
  */
 int _pow_recursion(int x, int y)
 {
