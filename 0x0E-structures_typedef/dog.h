23b70e5e36fc90be1fe7c64f83f0921f9bb5b0ca
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-07-05 01:20:27 +0300

    structures

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
+
 /**
  * struct dog - Struct dog
  * @name: name
@@ -7,11 +8,16 @@
  * @owner:  owner
  * Description: This struct is for dog
  */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
