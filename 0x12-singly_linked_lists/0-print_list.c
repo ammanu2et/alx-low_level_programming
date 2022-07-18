e12ea81ea78caffd3cacd4d6a699d7ea0e1a5e90
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-07-18 22:07:29 +0300

    linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,9 +8,9 @@
 
 size_t print_list(const list_t *h)
 {
-size_t first;
+size_t nelem;
 
-first = 0;
+nelem = 0;
 while (h != NULL)
 {
 if (h->str == NULL)
@@ -18,7 +18,7 @@ printf("[%d] %s\n", 0, "(nil)");
 else
 printf("[%d] %s\n", h->len, h->str);
 h = h->next;
-first++;
+nelem++;
 }
-return (first);
+return (nelem);
 }
