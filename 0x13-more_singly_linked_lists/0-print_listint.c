59dcbf6a547c01832a4740037ebbe045ea1661e9
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-07-19 13:39:37 +0300

    more linked lists

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
 
 size_t print_listint(const listint_t *h)
 {
-size_t numnodes = 0;
+size_t nnodes = 0;
 
 while (h != NULL)
 {
 printf("%d\n", h->n);
 h = h->next;
-numnodes++;
+nnodes++;
 }
-return (numnodes);
+return (nnodes);
 }
