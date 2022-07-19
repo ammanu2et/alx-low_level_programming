3a84f070d5da27b662b395c03f6dcf2758ad61a9
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-07-19 22:40:30 +0300

    more linked lists

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Realease the memory allocated for a list
+ * @head: a pointer to the first node of the list to free
+ */
+void free_listint2(listint_t **head)
+{
+listint_t *tmp;
+
+if (!(head && *head))
+return;
+while (*head != NULL)
+{
+tmp = (*head)->next;
+free(*head);
+*head = tmp;
+}
+}
