57c995f2d1adb80b6f9af8563e5f481824f3ca08
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-07-19 16:19:58 +0300

    more linked lists

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * listint_len - prints the number of elements in the list.
+ * @h: hade of the list.
+ * Return: the count value.
+ */
+
+size_t listint_len(const listint_t *h)
+{
+int count = 0;
+while (h != NULL)
+{
+count++;
+h = h.getNext();
+}
+return (count);
+}
