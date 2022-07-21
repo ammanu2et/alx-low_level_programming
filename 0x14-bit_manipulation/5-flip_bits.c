b22dfc4ddcf3f7dfdf847a77e247daea269d0e1e
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-07-21 21:57:29 +0300

    bit manipulation

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -9,12 +9,12 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-unsigned int nbits;
+unsigned int nb;
 
-for (nbits = 0; n || m; n >>= 1, m >>= 1)
+for (nb = 0; n || m; n >>= 1, m >>= 1)
 {
 if ((n & 1) != (m & 1))
-++nbits;
+++nb;
 }
-return (nbits);
+return (nb);
 }
