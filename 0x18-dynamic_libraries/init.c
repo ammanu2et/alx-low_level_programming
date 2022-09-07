a743afe5d1799b202113ce97c495f8ec163e3297
ammanu2et <105004972+ammanu2et@users.noreply.github.com>
2022-09-07 15:24:42 +0300

    dynamic libraries

--- 0x18-dynamic_libraries/init.c
+++ 0x18-dynamic_libraries/init.c
@@ -1,19 +1,8 @@
 /**
- * init - initialize a variable of type struct dog
- *@d: array
- *@name: name
- *@age: age
- *@owner: owner
- * Description: This struct is for dog
+ * init - function
  */
 
-void init(char *name, float age, char *owner)
+void init(void)
 {
-struct dog
-	{
-		d->name = name;
-		d->age = age;
-		d->owner = owner;
-	}
 
 }
